274130d8916616bb2c5d1cb493fda4dcb91e2010
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-23 02:40:54 -0300

    agrego modo oscuro

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,21 +11,27 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComponent()
+            ComposeTheme {
+                MyComponent()
+            }
         }
     }
 }
@@ -32,7 +39,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyComponent() {
     Row(
-        modifier = Modifier.padding(start = 8.dp)
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+            .padding(start = 8.dp)
     ) {
         MyImage()
         MyTexts()
@@ -40,32 +49,44 @@ fun MyComponent() {
 }
 
 @Composable
-fun MyImage(){
+fun MyImage() {
     Image(
         painterResource(R.drawable.ic_launcher_foreground),
         contentDescription = "Mi imagen de prueba",
         modifier = Modifier
             .clip(CircleShape)
-            .background(Color.Gray)
+            .background(MaterialTheme.colorScheme.primary)
+            .size(64.dp)
     )
 }
 
 @Composable
 fun MyTexts() {
     Column(modifier = Modifier.padding(start = 8.dp)) {
-        MyText("¡Hola Jetpack Compose!")
+        MyText(
+            "¡Hola Jetpack Compose!",
+            MaterialTheme.colorScheme.onBackground,
+            MaterialTheme.typography.titleLarge
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        MyText("¿Preparado?")
+        MyText(
+            "¿Preparado?",
+            MaterialTheme.colorScheme.onBackground,
+            MaterialTheme.typography.titleMedium
+        )
     }
 }
 
 @Composable
-fun MyText(text: String) {
-    Text(text)
+fun MyText(text: String, color: Color, style: TextStyle) {
+    Text(text, color = color, style = style)
 }
 
-@Preview
+@Preview()
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewComponents() {
-    MyComponent()
+    ComposeTheme {
+        MyComponent()
+    }
 }
