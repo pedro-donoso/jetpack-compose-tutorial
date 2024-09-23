11c737e62a2e46e338882dfd7597795e12b4bee2
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-23 01:18:31 -0300

    agrego imagen

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,26 +3,43 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyTexts()
+            MyComponent()
         }
+    }
+}
+
+@Composable
+fun MyComponent() {
+    Row() {
+        MyImage()
+        MyTexts()
     }
 }
 
 
 @Composable
+fun MyImage(){
+    Image(painterResource(R.drawable.ic_launcher_foreground),
+        contentDescription = "Mi imagen de prueba")
+}
+
+@Composable
 fun MyTexts() {
     Column {
-        Text("Hola")
-        Text("Mundo")
+        Text("¡Hola Jetpack Compose!")
+        Text("¿Preparado?")
     }
 }
 
@@ -33,6 +50,6 @@ fun MyText(text: String) {
 
 @Preview
 @Composable
-fun PreviewTexts() {
-    MyTexts()
+fun PreviewComponents() {
+    MyComponent()
 }
