2f905f16f9479dba255c3a52301c2a1168ea259d
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-23 02:15:41 -0300

    agrego fondo imagen

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,24 +31,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyComponent() {
-    Row() {
+    Row(
+        modifier = Modifier.padding(start = 8.dp)
+    ) {
         MyImage()
         MyTexts()
     }
 }
 
-
 @Composable
 fun MyImage(){
-    Image(painterResource(R.drawable.ic_launcher_foreground),
-        contentDescription = "Mi imagen de prueba")
+    Image(
+        painterResource(R.drawable.ic_launcher_foreground),
+        contentDescription = "Mi imagen de prueba",
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(Color.Gray)
+    )
 }
 
 @Composable
 fun MyTexts() {
-    Column {
-        Text("¡Hola Jetpack Compose!")
-        Text("¿Preparado?")
+    Column(modifier = Modifier.padding(start = 8.dp)) {
+        MyText("¡Hola Jetpack Compose!")
+        Spacer(modifier = Modifier.height(16.dp))
+        MyText("¿Preparado?")
     }
 }
 
