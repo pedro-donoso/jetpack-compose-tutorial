43d52c50c101d0812b187b666833fff5cb9a074b
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-23 00:59:41 -0300

    first commit

---
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hola Jetpack Compose")
+        }
+    }
+}
+
